a6156cdf942dd99893eef4b81ba09beb8ebcc2b7
Praveenkumar <praveen1050208@gmail.com>
2020-03-11 17:30:35 +0530

    Update transformer.py

--- scripts/transformer.py
+++ scripts/transformer.py
@@ -4,7 +4,15 @@ import numpy as np
 from hyper_parameters import h_parms
 from configuration import config
 
+'''Positional encoding is added to give the model some information about the 
+   relative position of the words in the sentence.Nearby tokens will have 
+   similar position-encoding vectors. Any relative position encoding can be written 
+   as a linear function of the current position. Raw angles are not a good model 
+   input (they're either unbounded, or discontinuous) so take the sine and cosine'''
 def get_angles(pos, i, d_model):
+  '''Get angle rate for the projected embedding output (d_model)
+     and multiply that with the target vocab size
+  '''
   angle_rates = 1 / np.power(10000, (2 * (i//2)) / np.float32(d_model))
   return pos * angle_rates
 
@@ -25,30 +33,42 @@ def positional_encoding(position, d_model):
 
 
 def create_padding_mask(seq):
+  '''The mask indicates where pad value 0 is present.
+     it outputs a 1 at those locations, and a 0 otherwise.
+  '''
   seq = tf.cast(tf.math.equal(seq, 0), tf.float32)
-  
   # add extra dimensions so that we can add the padding
   # to the attention logits.
-  return seq[:, tf.newaxis, tf.newaxis, :]  # (batch_size, 1, 1, seq_len)
+
+  # (batch_size, 1, 1, seq_len)
+  return seq[:, tf.newaxis, tf.newaxis, :]  
+
+def create_look_ahead_mask(size):
+  '''look-ahead mask is used to mask the future tokens in a sequence
+     i.e to predict the third word, only the first and second word will be used
+  '''
+            #lower_triangular_matrix
+  mask = 1 - tf.linalg.band_part(tf.ones((size, size)), -1, 0)  
+  # (seq_len, seq_len)
+  return mask  
 
 def create_masks(inp, tar):
   # Encoder padding mask
-  enc_padding_mask = create_padding_mask(inp)  #output 1 if padded 0 is present else 0
+  enc_padding_mask = create_padding_mask(inp)  
   
   # Used in the 2nd attention block in the decoder.
   # This padding mask is used to mask the encoder outputs.
   dec_padding_mask = create_padding_mask(inp)
   # Used in the 1st attention block in the decoder.
   # It is used to pad and mask future tokens in the input received by 
-  # the decoder.allows decoder to attend to all positions in the decoder up to and including that position(refer architecture)
+  # the decoder.allows decoder to attend to all positions in the decoder up to and 
+  # including that position(refer architecture)
   dec_target_padding_mask = create_padding_mask(tar)
   look_ahead_mask = create_look_ahead_mask(tf.shape(tar)[1])
   combined_mask = tf.maximum(dec_target_padding_mask, look_ahead_mask)
   return enc_padding_mask, combined_mask, dec_padding_mask
 
-def create_look_ahead_mask(size):
-  mask = 1 - tf.linalg.band_part(tf.ones((size, size)), -1, 0)  #(1 - lower_triangular_matrix)
-  return mask  # (seq_len, seq_len)
+
 
 def scaled_dot_product_attention(q, k, v, mask):
   """Calculate the attention weights.
@@ -81,6 +101,16 @@ def scaled_dot_product_attention(q, k, v, mask):
   output = tf.matmul(attention_weights, v)  # (..., seq_len_q, depth_v)
   return output, attention_weights
 
+'''
+MultiHeadAttention:-
+
+The scaled_dot_product_attention defined above is applied to each head (broadcasted for efficiency). 
+An appropriate mask must be used in the attention step. The attention output for each head is then concatenated 
+and put through a final Dense layer.Instead of one single attention head, Q, K, and V are split into multiple 
+heads because it allows the model to jointly attend to information at different positions from different 
+representational spaces. After the split each head has a reduced dimensionality, 
+so the total computation cost is the same as a single head attention with full dimensionality.
+'''
 class MultiHeadAttention(tf.keras.layers.Layer):
   def __init__(self, d_model, num_heads):
     super(MultiHeadAttention, self).__init__()
@@ -143,10 +173,10 @@ class MultiHeadAttention(tf.keras.layers.Layer):
         
     return output, attention_weights
 
-# arg1 (batch_size, seq_len, dff)
-# arg2 (batch_size, seq_len, d_model)
+
 def point_wise_feed_forward_network(d_model, dff):
-  
+  # d_model (batch_size, seq_len, dff)
+  # dff (batch_size, seq_len, d_model)
   return tf.keras.Sequential([
       tf.keras.layers.Dense(dff, 
                             activation='relu', 
@@ -156,7 +186,6 @@ def point_wise_feed_forward_network(d_model, dff):
                             kernel_regularizer = tf.keras.regularizers.l2(h_parms.l2_norm),
                             )
   ])
-
 
 
 class DecoderLayer(tf.keras.layers.Layer):
@@ -181,11 +210,19 @@ class DecoderLayer(tf.keras.layers.Layer):
            look_ahead_mask, padding_mask):
     
     
-    attn1, attn_weights_block1 = self.mha1(x, x, x, look_ahead_mask)  
+    attn1, attn_weights_block1 = self.mha1(x, 
+                                           x, 
+                                           x, 
+                                           look_ahead_mask
+                                           )  
     attn1 = self.dropout1(attn1, training=training)
     out1 = self.layernorm1(attn1 + x)
     attn2, attn_weights_block2 = self.mha2(
-        enc_output, enc_output, out1, padding_mask)  
+                                           enc_output, 
+                                           enc_output, 
+                                           out1, 
+                                           padding_mask
+                                           )  
     attn2 = self.dropout2(attn2, training=training)
     # (batch_size, target_seq_len, d_model)
     out2 = self.layernorm2(attn2 + out1)  
@@ -219,12 +256,12 @@ class Pointer_Generator(tf.keras.layers.Layer):
     # vocab_dist (batch_size, tar_seq_len, target_vocab_size)   
     vocab_dist_ = tf.math.softmax(final_output, axis=-1)
     vocab_dist = p_gen * vocab_dist_ 
-    # attention_dist (batch_size, tar_seq_len, inp_seq_len)
     # attention_weights is 4D so taking mean of the second dimension(i.e num_heads)
     if h_parms.mean_attention_heads:
       attention_weights_ = tf.reduce_mean(attention_weights, axis=1)
     else:
       attention_weights_ = attention_weights[:, -1, :, :]
+    # attention_dist (batch_size, tar_seq_len, inp_seq_len)
     attention_dist = tf.math.softmax(attention_weights_, axis=-1)
     # updates (batch_size, tar_seq_len, inp_seq_len)
     updates = (1 - p_gen) * attention_dist
@@ -241,7 +278,6 @@ class Pointer_Generator(tf.keras.layers.Layer):
     indices = tf.stack([i1, i2, indices_x], axis=-1)
     # copy_probs (batch_size, tar_seq_len, target_vocab_size)
     copy_probs = tf.scatter_nd(indices, updates, shape)   
-    # Added a small value to ensure numerical stability
     combined_probs = vocab_dist + copy_probs
     combined_logits = tf.math.log(combined_probs)
     return combined_logits
@@ -275,10 +311,10 @@ class Decoder(tf.keras.layers.Layer):
     x += self.pos_encoding[:, :seq_len, :]
     x = self.dropout(x, training=training)
     for i in range(self.num_layers):
-      x, block1, block2 = self.dec_layers[i](x, 
-					     enc_output, 
+      x, block1, block2 = self.dec_layers[i](x,
+					     enc_output,
 					     training,
-                                             look_ahead_mask, 
+                                             look_ahead_mask,
                                              padding_mask)
       attention_weights['decoder_layer{}_block1'.format(i+1)] = block1
       attention_weights['decoder_layer{}_block2'.format(i+1)] = block2
