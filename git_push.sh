a07bc0db51b8b73a36eceb4c6db55f54a7d02a1b
Praveen kumar <praveen1050208@gmail.com>
2020-03-23 11:14:03 +0530

    house keeping

--- git_push.sh
+++ git_push.sh
@@ -1,5 +1,8 @@
+rm -rf /d/Summarize_and_translate_old_repos/Summarize_and_translate/
+cd /d/Summarize_and_translate_old_repos
+git clone https://github.com/praveenjune17/Summarize_and_translate.git
+cd /d/Summarize_and_translate
 git add -A
 git commit -m 'house keeping'
 git pull origin master
 git push origin master
-:
