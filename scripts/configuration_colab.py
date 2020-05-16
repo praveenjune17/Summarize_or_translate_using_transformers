bf6b77ae3cf4af61e94bb15ae7ca359f9db75cba
Praveenkumar <praveen1050208@gmail.com>
2020-05-16 12:34:20 +0530

    Update configuration_colab.py

--- scripts/configuration_colab.py
+++ scripts/configuration_colab.py
@@ -2,8 +2,7 @@
 import os
 import platform
 from bunch import Bunch
-from check_rules import (adhere_task_rules, 
-        assert_config_values, set_memory_growth)
+from check_rules import check_and_assert_config
 
 unit_test = {
       'check_evaluation_pipeline' : False,
@@ -29,13 +28,13 @@ model_parms = {
      'add_pointer_generator': True,
      'd_model': 768,                  # the projected word vector dimension
      'dff': 1024,                      # feed forward network hidden parameters
-     'input_pretrained_bert_model': 'distilroberta-base',  #distilroberta-base, #bert-base-uncased , #google/electra-small-discriminator
+     'input_pretrained_model': 'distilroberta-base',  #distilroberta-base, #bert-base-uncased , #google/electra-small-discriminator
      'input_seq_length': 50,
      'model' : 'bertified_transformer',#bertified_transformer or transformer
      'num_heads': 8,                  # the number of heads in the multi-headed attention unit
      'num_layers': 8,                 # number of transformer blocks
      'target_language' : 'ta',
-     'target_pretrained_bert_model' : 'distilbert-base-multilingual-cased',#'bert-base-uncased',
+     'target_pretrained_model' : 'distilbert-base-multilingual-cased',#'bert-base-uncased',
                                                                      #'bert-base-multilingual-cased',
                                                                     #'distilbert-base-multilingual-cased'
      'target_seq_length': 20,
@@ -49,16 +48,16 @@ training_parms = {
      'enable_jit' : False,
      'eval_after_steps' : 5000,              # Evaluate after these many training steps
      'gradient_accumulation_steps': 9,   
-     'last_recorded_value': None,
+     'last_recorded_value': 0.5459,
      'min_train_loss' : 1.0,
      'monitor_metric' : 'unified_metric',
      'run_tensorboard': True,
      'samples_to_train' : -1,
-     'samples_to_validate' : 100,
+     'samples_to_validate' : 126,            
      'start_evaluate_when' : 5.0,           # run evaluation when loss reaches 10
      'steps_to_print_training_info': 100,      # print training progress per number of batches specified
      'tfds_name' : 'en_tam_parallel_text',            #cnn_dailymail,en_tam_parallel_text     # tfds dataset to be used
-     'tolerance' : 0,
+     'init_tolerance' : 0,
      'tolerance_threshold': 7,          # Stop training after the threshold is reached
      'tokens_per_batch' : 4050,
      'use_custom_tokenizer' : None,
@@ -66,23 +65,15 @@ training_parms = {
      'write_batch1_predictions': True           # write the first batch of validation set summary to a file
      }                                    
 
-# Special Tokens
-special_tokens = {
-    'input_CLS_ID' : 101,                            #assumed to be BERT ids
-    'input_SEP_ID' : 102,                            #assumed to be BERT ids
-    'MASK_ID' : 103,
-    'PAD_ID' : 0,
-    }
-
 inference_decoder_parms = {
     'beam_size': 1,              
     'draft_decoder_type' : 'greedy',     # 'greedy', 'only_beam_search', 'topktopp' --> topktopp filtering + beam search
     'length_penalty' : 1,
     'num_parallel_calls' : -1,
-    'refine_decoder_type' : 'greedy',
+    'refine_decoder_type' : 'greedy',     # 'greedy', 'topktopp' --> beam search not possible
     'softmax_temperature' : 1,
-    'top_p' : 0.9, 
-    'top_k' : 5                         
+    'top_p' : 1, 
+    'top_k' : 0                         
     }
 
 h_parms = {
@@ -99,7 +90,7 @@ h_parms = {
 
 dataset_name = training_parms['tfds_name']
 model = model_parms['model']
-core_path = "/content/drive/My Drive/"#os.getcwd()
+core_path = "/content/drive/My Drive/"
 path_seperator = '\\' if platform.system() == 'Windows' else '/'
 file_path = {
         'best_ckpt_path' : os.path.join(core_path, f"best_checkpoints{path_seperator}{dataset_name+'_'+model}{path_seperator}"),  
@@ -111,7 +102,7 @@ file_path = {
         'log_path' : os.path.join(core_path, f"created_files{path_seperator}{dataset_name+'_'+model}{path_seperator}tensorflow.log"),
         'output_seq_vocab_path' : os.path.join(core_path, f"TFDS_vocab_files{path_seperator}{dataset_name}{path_seperator}vocab_ta"),
         'output_sequence_write_path' : os.path.join(core_path, f"created_files{path_seperator}{dataset_name+'_'+model}{path_seperator}summaries{path_seperator}"),
-        'serialized_tensor_path' : os.path.join("/content/drive/My Drive/saved_serialized_tensor_ta"),
+        'serialized_tensor_path' : os.path.join(core_path, 'saved_serialized_tensor_'+ model_parms['target_language']),
         'tensorboard_log' : os.path.join(core_path, f"created_files{path_seperator}{dataset_name+'_'+model}{path_seperator}tensorboard_logs{path_seperator}"),
         'tfds_data_dir' : os.path.join(core_path, f'Tensorflow_datasets{path_seperator}{dataset_name}_dataset'),
         'tfds_data_version' : None,
@@ -121,18 +112,8 @@ file_path = {
 config = Bunch(model_parms)
 config.update(unit_test)
 config.update(training_parms)
-config.update(special_tokens)
 config.update(inference_decoder_parms)
 config.update(h_parms)
 config.update(file_path)
 
-#set GPU memory growth
-_ = set_memory_growth()
-config, source_tokenizer, target_tokenizer = adhere_task_rules(config)
-config = assert_config_values(config)
-
-# Special Tokens 
-config['PAD_ID']  = target_tokenizer.pad_token_id
-config['CLS_ID']  = target_tokenizer.cls_token_id
-config['MASK_ID'] = target_tokenizer.mask_token_id
-config['SEP_ID']  = target_tokenizer.sep_token_id
+config, source_tokenizer, target_tokenizer = check_and_assert_config(config)
