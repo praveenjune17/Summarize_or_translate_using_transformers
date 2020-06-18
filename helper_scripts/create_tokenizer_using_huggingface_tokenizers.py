d40dbe8ff2d9bf0caac3b0e8db059949948c1509
Praveenkumar <praveen1050208@gmail.com>
2020-06-18 14:44:26 +0530

    Update create_tokenizer_using_huggingface_tokenizers.py

--- helper_scripts/create_tokenizer_using_huggingface_tokenizers.py
+++ helper_scripts/create_tokenizer_using_huggingface_tokenizers.py
@@ -2,23 +2,30 @@
 Borrowed from https://huggingface.co/blog/how-to-train
 '''
 
-from tokenizers import ByteLevelBPETokenizer
+from tokenizers import ByteLevelBPETokenizer, Tokenizer
 from pathlib import Path
+import os
 en_tokenizer = ByteLevelBPETokenizer()
 ta_tokenizer = ByteLevelBPETokenizer()
-'''
-en_path1 = [str(x) for x in Path("D:/Local_run/Hugging_face_tokenizers_input/").glob("**/*_en.*")]
-en_path2 = [str(x) for x in Path("D:/Local_run/Hugging_face_tokenizers_input/").glob("**/*.en")]
-en_path3 = [str(x) for x in Path("D:/Local_run/Hugging_face_tokenizers_input/").glob("**/*.en_??")]
-en_paths = en_path1+en_path2+en_path3
-ta_path1 = [str(x) for x in Path("D:/Local_run/Hugging_face_tokenizers_input/").glob("**/*_ta.*")]
-ta_path2 = [str(x) for x in Path("D:/Local_run/Hugging_face_tokenizers_input/").glob("**/*.ta*")]
-ta_paths = ta_path1+ta_path2
-'''
-en_paths = 'Path to the files containing english documents'
-ta_paths =  'Path to the files containing tamil documents'
-en_tokenizer_path = 'en_tokenizer'
-ta_tokenizer_path = 'ta_tokenizer'
+
+file_path = '/content/drive/My Drive/Tensorflow_datasets/en_tam_parallel_text_dataset/downloads/extracted/'
+en_path1 = [str(x) for x in Path(file_path).glob("**/*_en.*")]
+en_path2 = [str(x) for x in Path(file_path).glob("**/*.en")]
+en_path3 = [str(x) for x in Path(file_path).glob("**/*.en_??")]
+en_paths = list(set(en_path1+en_path2+en_path3))
+ta_path1 = [str(x) for x in Path(file_path).glob("**/*_ta.*")]
+ta_path2 = [str(x) for x in Path(file_path).glob("**/*.ta*")]
+ta_paths = list(set(ta_path1+ta_path2))
+
+
+new_ta_path = []
+for f in ta_paths:
+  if not os.path.isdir(f):
+    new_ta_path.append(f)
+#en_paths = 'Path to the files containing english documents'
+#ta_paths =  'Path to the files containing tamil documents'
+en_tokenizer_path = 'en_tokenizer-vocab.json'
+ta_tokenizer_path = 'ta_tokenizer-vocab.json'
 # Customize training
 en_tokenizer.train(files=en_paths, vocab_size=8300, min_frequency=2, special_tokens=[
     "<CLS>",
@@ -29,21 +36,21 @@ en_tokenizer.train(files=en_paths, vocab_size=8300, min_frequency=2, special_tok
 ])
 print('en completed')
 # Customize training
-ta_tokenizer.train(files=ta_paths, vocab_size=8300, min_frequency=2, special_tokens=[
+ta_tokenizer.train(files=new_ta_path, vocab_size=8300, min_frequency=2, special_tokens=[
     "<CLS>",
     "<pad>",
     "<SEP>",
     "<UNK>",
     "<MASK>",
 ])
-en_tokenizer.save(".", f"{en_tokenizer_path}")
-ta_tokenizer.save(".", f"{ta_tokenizer_path}")
+print('ta completed')
+en_tokenizer.save(en_tokenizer_path)
+ta_tokenizer.save(ta_tokenizer_path)
+en_tokenizer = Tokenizer.from_file(en_tokenizer_path)
+ta_tokenizer = Tokenizer.from_file(ta_tokenizer_path)
 tamil_text = 'அதனை நிரூபிப்பதுபோல் இருக்குமாம் படம்'
 english_text = 'This movie will prove that'
 id_1 = ta_tokenizer.encode(tamil_text)
 assert (ta_tokenizer.decode(id_1.ids)==tamil_text), 'mismatch in tamil tokenizer encoding'
 id_2 = en_tokenizer.encode(english_text)
 assert (en_tokenizer.decode(id_2.ids)==english_text), 'mismatch in english tokenizer encoding'
-
-en_tokenizer = ByteLevelBPETokenizer(f'.\\{en_tokenizer_path}-vocab.json', f'.\\{en_tokenizer_path}-merges.txt')
-ta_tokenizer = ByteLevelBPETokenizer(f'.\\{ta_tokenizer_path}-vocab.json', f'.\\{ta_tokenizer_path}-merges.txt')
